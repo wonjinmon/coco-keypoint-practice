6f9a0f96b34547a8b772bedc906df12a1efcc056
wonjinmon <own1045@gmail.com>
2023-09-25 22:18:13 +0900

    second commit

--- README.md
+++ README.md
@@ -2,23 +2,44 @@
 
 # Should be a format
 
+```
+infant-dataset/
+    images/
+        17962/
+            126524578_1.jpg
+            ...
+        18027/
+            126499894_1.jpg
+            ...
+        18028/
+        ...
+    annotations.json
+```
+
+
 ```json
 {
-    "image": [
+    "images": [
         {
             "id": "int",
             "width": "int",
             "height": "int",
-            "file_name": "str"
+            "file_name": "images/17962/1265~_1.jpg",
+            "license": "int", 
+            "flickr_url": "str", 
+            "coco_url": "str", 
+            "date_captured": "datetime",
         }
     ],
-    "annotation": [
+    "annotations": [
         {
             "id": "int",
             "image_id": "int",
             "category_id": "int",
-            "segmentation": "RLE or [polygon]", "area": "float", 
-            "bbox": "[x,y,width,height]", "iscrowd": "0 or 1",
+            "segmentation": "RLE or [polygon]", 
+            "area": "float", 
+            "bbox": "[x,y,width,height]", 
+            "iscrowd": "0 or 1",
             "keypoints": "[x1, y1, v1,...]",
             "num_keypoints": "int"
         }
--- convert_format.py
+++ convert_format.py
@@ -4,29 +4,24 @@ import os
 import glob
 import shutil
 import sys
+import re
+
 from pathlib import Path
 from collections import defaultdict
+from PIL import Image
+from tqdm import tqdm
 
-# !TODO 
+# !TODO
 # 1. infant-dataset/ annotations.json 파일 하나를 생성한다.
 # 1.1 original-data에 있는 숫자.json 파일들을 하나로 합쳐야 된다.
-# 1.1 id가 중복되어서 중복안되게끔 생성해줘야하고, 파일 경로 상대로 다시 셋팅해줘되고, 
+# 1.1 id가 중복되어서 중복안되게끔 생성해줘야하고, 파일 경로 상대로 다시 셋팅해줘되고,
 # 그다음에 그 경로대로 original/ 폴더에 있는 이미지 infant-dataset 경로 복사해주기
 # 2. 키 포인트 추출
-# 2.1 annotations > keypoints이용해서 x,y 좌표얻음
-# 2.2 그 좌표를 통해 4개 좌표 뽑고 박스치기
+# 2.1 annotations > keypoints이용해서 x,y 좌표얻음 
+# 2.2 그 좌표를 통해 4개 좌표 뽑고 박스치기 (# x, y, w, h)
 
 
-# # json 수정할 파일 이름 뽑아보기
-# with open("infant-dataset/images/17962.json") as f:
-#     data = json.loads(f.read())
-# # print(type(data["images"][0]))
-# for i in range(len(data["images"])):
-#     file_name = data["images"][i]["file_name"]  # json에서 파일이름을 뽑은거
-#     print(file_name)
 
-
-import re
 """
 {
     "images": [
@@ -84,53 +79,178 @@ import re
 
 """
 
-path = "./infant-dataset/images/PROJ-6312_keypoint_2023-08-04/17962/126524578/*.jpg"
+with open("original-data/17962.json", "r") as f:
+    annots = json.load(f)
 
-paths = glob.glob("./original-data/*/*/*")
-print(f"{Path(paths[0]).parts[-2]}.json")
+annot_dict = defaultdict(list)
 
-annot_by_ids = defaultdict(list)
-annot_by_ids[1].append({
-    "images": {},
-    "annotations": {}
-})
-annot_by_ids[2].append({"what": "the"})
-print(annot_by_ids)
-print(annot_by_ids[1])
-with open("infant-dataset/annotations.json", "w") as f:
-    json.dumps(f)
+for annot in annots["annotations"]:
+    annot_dict[annot["id"]].append(annot)
 
-sys.exit()
-
-ids = 1
-
-path_list = path.split("/")
-
-name_list = sorted(
-    glob.glob(path), key=lambda x: int(re.sub(r"[^0-9]", "", x.split("_")[-1]))
+new_annot_file = {"images": [], "annotations": [], "categories": []}
+new_annot_file["categories"].append(
+    {
+        "id": 1, "name": "infant", "supercategory": None,
+        "keypoints": [
+            "head_top",
+            "right_eye",
+            "left_eye",
+            "nose",
+            "mouth",
+            "right_ear",
+            "left_ear",
+            "chin",
+            "notch_of_sternum",
+            "xiphoid_process",
+            "right_shoulder_joint",
+            "left_shoulder_joint",
+            "right_elbow_joint",
+            "left_elbow_joint",
+            "right_wrist_joint",
+            "left_wrist_joint",
+            "mid hip",
+            "right_pelvis, ASIS",
+            "left_pelvis, ASIS",
+            "public_symphysis",
+            "right_hip_joint",
+            "left_hip_joint",
+            "right_knee_joint",
+            "left_knee_joint",
+            "right_ankle_joint",
+            "left_ankle_joint"
+        ],
+        "skeleton": [
+        ]
+    },
 )
 
-new_name_list = []
-for i in range(len(name_list)):
-    img_dir = path_list[-2]
-    a = name_list[i].split("_")[-1]
-    img_name = img_dir + "_" + a
-    # print(img_name)
-    new_name_list.append(img_name)
-# print(new_name_list)
 
-# 뽑은 파일 이름으로 json 덮어쓰기
-with open("infant-dataset/images/17962.json") as f:
-    data = json.loads(f.read())
+annot_id = 1
+for image_info in tqdm(annots["images"]):
+    file_name = f"original-data/images/17962/126524578/126524578_{image_info['id']}.jpg" 
+    modified_path = file_name.split('/')
+    del modified_path[-2]
+    modified_path = '/'.join(modified_path)
+    w, h = Image.open(file_name).convert("RGB").size
+    image_result = {
+        "id": image_info["id"],
+        "width": w,
+        "height": h,
+        "file_name": modified_path,
+    }
+    new_annot_file["images"].append(image_result)
+    for annot in annot_dict[image_info["id"]]:
+        kp = np.array(annot["keypoints"]).reshape(-1, 3)
+        x1, y1, x2, y2 = kp[:, 0].min(), kp[:, 1].min(), kp[:, 0].max(), kp[:, 1].max()
+        annot_result = {
+            "id": annot_id,
+            "image_id": image_info["id"],
+            "category_id": 1,
+            "bbox": [[x1, y1, x2, y2]],  
+            "area": (x2 - x1) * (y2 - y1),
+            "iscrowd": 0,
+            "segmentations": [],
+            "keypoints": annot["keypoints"],
+            "num_keypoints": int(kp.shape[0]),
+        }
+        new_annot_file["annotations"].append(annot_result)
+    annot_id += 1
+    
+with open("infant-dataset/annotations.json", "w", encoding="utf-8") as f:
+    json.dump(new_annot_file, f, indent="\t")
 
-for i in range(len(data["images"])):
-    data["images"][i]["file_name"] = new_name_list[i]
+# paths = glob.glob("./original-data/*/*/*/*")
+# #
+
+# # img_names = []
+
+# # for i in range(1,6):
+# #     for val in path_data[f"path_data{i}"]:
+# #         print(val)
+
+# # 상단경로 얻기
+# path_names = []
+# for i in range(1, 1000, 200):
+#     p1, p2 = Path(paths[i]).parts[-3:-1]
+#     path_names.append(p1 + "/" + p2)
+# # >>> ['17962/126524578', '18027/126499894', '18028/126500381', '18030/126507318', '18222/126720846']
+
+# # path_1, path_2 = Path(paths[0]).parts[-3:-1]
+# # path_17962 = path_1 + '/' + path_2
 
 
-print(data["images"][0]["file_name"])
-print(data["images"][10]["file_name"])
+# # jpg만 얻기
+# img_names = []
+# for i in range(0, 999, 200):
+#     imgs = []
+#     for j in range(i, i + 200):
+#         imgs.append(Path(paths[j]).parts[-1])
+#     imgs = sorted(imgs, key=lambda x: int(re.sub(r"[^0-9]", "", x.split("_")[-1])))
+#     img_names += imgs
+# # >>> 5 x 200 ea
 
-with open("infant-dataset/images/17962.json", "w", encoding="utf-8") as f:
-    json.dump(data, f, indent="\t")
+# # img_17962 = []
+# # for i in range(200):
+# #     img_17962.append(Path(paths[i]).parts[-1])
+# # img_17962 = sorted(img_17962, key=lambda x: int(re.sub(r"[^0-9]", "", x.split("_")[-1])))
+
+# # name_list = sorted(
+# #     qwer, key=lambda x: int(re.sub(r"[^0-9]", "", x.split("_")[-1]))
+# # )
 
 
+# # 최종경로(file_name) 얻기
+# file_names = []
+# for idx, i in enumerate(range(0, 999, 200)):
+#     for name in img_names[i : i + 200]:
+#         file_names.append(path_names[idx] + "/" + name)
+# # print(file_names)
+
+
+# print(Path(paths[0]).parts[-3])
+# # json 파일 이름
+# json_file_names = []
+# for i in range(5):
+#     json_file_name = f"{Path(paths[i]).parts[-3]}.json"
+#     json_file_names.append(json_file_name)
+# print(json_file_names)
+
+# sys.exit()
+
+# annot_by_ids = defaultdict(list)
+# annot_by_ids[1].append({"images": {}, "annotations": {}})
+# annot_by_ids[2].append({"what": "the"})
+# ids = 1
+
+# path_list = path.split("/")
+
+# print(len(paths))
+# path_data = {'path_data1': paths[:200],
+#              'path_data2': paths[200:400],
+#              'path_data3': paths[400:600],
+#              'path_data4': paths[600:800],
+#              'path_data5': paths[800:]}
+# path_data["path_data1"]
+
+# new_name_list = []
+# for i in range(len(name_list)):
+#     img_dir = path_list[-2]
+#     a = name_list[i].split("_")[-1]
+#     img_name = img_dir + "_" + a
+
+#     new_name_list.append(img_name)
+
+
+# # 뽑은 파일 이름으로 json 덮어쓰기
+# with open("infant-dataset/images/17962.json") as f:
+#     data = json.loads(f.read())
+
+# for i in range(len(data["images"])):
+#     data["images"][i]["file_name"] = new_name_list[i]
+
+
+# print(data["images"][0]["file_name"])
+# print(data["images"][10]["file_name"])
+
+# with open("infant-dataset/images/17962.json", "w", encoding="utf-8") as f:
+#     json.dump(data, f, indent="\t")
