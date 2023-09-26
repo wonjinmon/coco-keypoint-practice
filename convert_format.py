7dde9ba78df8ce0be5690826d67b39c81970bc93
WonJin Hong <own1045@gmail.com>
2023-09-26 11:04:21 +0900

    modifying file path

--- convert_format.py
+++ convert_format.py
@@ -75,94 +75,118 @@ from tqdm import tqdm
     ],
     "num_keypoints": 25
 },
-
 """
+
+'''
+original-data/images/17962/126524578/126524578_1.jpg
+original-data/images/18027/126499894/126499894_1.jpg
+original-data/images/18028/126500381/126500381_1.jpg
+original-data/images/18030/126507318/126507318_1.jpg
+original-data/images/18222/126720846/126720846_1.jpg
+'''
+
+file_map = [
+    "original-data/images/17962/126524578/126524578_1.jpg",
+    "original-data/images/18027/126499894/126499894_1.jpg",
+    "original-data/images/18028/126500381/126500381_1.jpg",
+    "original-data/images/18030/126507318/126507318_1.jpg",
+    "original-data/images/18222/126720846/126720846_1.jpg"
+]
+# print(file_map[0].split('_')[1][0])
+print(glob.glob('./original-data/*/*/*/*'))
 json_files = glob.glob("./original-data/*.json")
 
-for json_file in json_files:
-    ...
+for json_file in range(len(json_files)):
+
+    with open(f"{json_files[json_file]}", "r") as f:
+        annots = json.load(f)  
+
+    annot_dict = defaultdict(list)
+
+    for annot in annots["annotations"]:
+        annot_dict[annot["id"]].append(annot)
+
+    new_annot_file = {"images": [], "annotations": [], "categories": []}
+    new_annot_file["categories"].append(
+        {
+            "id": 1,
+            "name": "infant",
+            "supercategory": None,
+            "keypoints": [
+                "head_top",
+                "right_eye",
+                "left_eye",
+                "nose",
+                "mouth",
+                "right_ear",
+                "left_ear",
+                "chin",
+                "notch_of_sternum",
+                "xiphoid_process",
+                "right_shoulder_joint",
+                "left_shoulder_joint",
+                "right_elbow_joint",
+                "left_elbow_joint",
+                "right_wrist_joint",
+                "left_wrist_joint",
+                "mid hip",
+                "right_pelvis, ASIS",
+                "left_pelvis, ASIS",
+                "public_symphysis",
+                "right_hip_joint",
+                "left_hip_joint",
+                "right_knee_joint",
+                "left_knee_joint",
+                "right_ankle_joint",
+                "left_ankle_joint",
+            ],
+            "skeleton": [],
+        },
+    )
 
 
-with open(f"{json_files[0]}", "r") as f:
-    annots = json.load(f)
+    annot_id = 1
+    for idx, image_info in tqdm(enumerate(annots["images"])):
+        print('')
+        print('idx', idx)
+        print("id",     image_info["id"])
+        print(file_map[idx].split("_")[-1])
+        # print(file_map[idx].split('_')[1][0])
+        file_name = file_map[idx].replace(file_map[idx].split('_')[-1], f'{image_info["id"]}.jpg')
+        print(file_name)
+        # file_name = f"original-data/images/17962/126524578/126524578_{image_info['id']}.jpg"
+        modified_path = file_name.split("/")
+        del modified_path[-2]
+        modified_path = "/".join(modified_path)
+        print(modified_path)
+    #     w, h = Image.open(file_name).convert("RGB").size
+    #     image_result = {
+    #         "id": image_info["id"],
+    #         "width": w,
+    #         "height": h,
+    #         "file_name": modified_path,
+    #     }
+    #     new_annot_file["images"].append(image_result)
+    #     for annot in annot_dict[image_info["id"]]:
+    #         kp = np.array(annot["keypoints"]).reshape(-1, 3)
+    #         x1, y1, x2, y2 = kp[:, 0].min(), kp[:, 1].min(), kp[:, 0].max(), kp[:, 1].max()
+    #         w, h = x2 - x1, y2 - y1
+    #         annot_result = {
+    #             "id": annot_id,
+    #             "image_id": image_info["id"],
+    #             "category_id": 1,
+    #             "bbox": [[x1, y1, w, h]],
+    #             "area": (x2 - x1) * (y2 - y1),
+    #             "iscrowd": 0,
+    #             "segmentations": [],
+    #             "keypoints": annot["keypoints"],
+    #             "num_keypoints": int(kp.shape[0]),
+    #         }
+    #         new_annot_file["annotations"].append(annot_result)
+    #     annot_id += 1
 
-annot_dict = defaultdict(list)
-
-for annot in annots["annotations"]:
-    annot_dict[annot["id"]].append(annot)
-
-new_annot_file = {"images": [], "annotations": [], "categories": []}
-new_annot_file["categories"].append(
-    {
-        "id": 1,
-        "name": "infant",
-        "supercategory": None,
-        "keypoints": [
-            "head_top",
-            "right_eye",
-            "left_eye",
-            "nose",
-            "mouth",
-            "right_ear",
-            "left_ear",
-            "chin",
-            "notch_of_sternum",
-            "xiphoid_process",
-            "right_shoulder_joint",
-            "left_shoulder_joint",
-            "right_elbow_joint",
-            "left_elbow_joint",
-            "right_wrist_joint",
-            "left_wrist_joint",
-            "mid hip",
-            "right_pelvis, ASIS",
-            "left_pelvis, ASIS",
-            "public_symphysis",
-            "right_hip_joint",
-            "left_hip_joint",
-            "right_knee_joint",
-            "left_knee_joint",
-            "right_ankle_joint",
-            "left_ankle_joint",
-        ],
-        "skeleton": [],
-    },
-)
-
-
-annot_id = 1
-for image_info in tqdm(annots["images"]):
-    file_name = f"original-data/images/17962/126524578/126524578_{image_info['id']}.jpg"
-    modified_path = file_name.split("/")
-    del modified_path[-2]
-    modified_path = "/".join(modified_path)
-    w, h = Image.open(file_name).convert("RGB").size
-    image_result = {
-        "id": image_info["id"],
-        "width": w,
-        "height": h,
-        "file_name": modified_path,
-    }
-    new_annot_file["images"].append(image_result)
-    for annot in annot_dict[image_info["id"]]:
-        kp = np.array(annot["keypoints"]).reshape(-1, 3)
-        x1, y1, x2, y2 = kp[:, 0].min(), kp[:, 1].min(), kp[:, 0].max(), kp[:, 1].max()
-        annot_result = {
-            "id": annot_id,
-            "image_id": image_info["id"],
-            "category_id": 1,
-            "bbox": [[x1, y1, x2, y2]],
-            "area": (x2 - x1) * (y2 - y1),
-            "iscrowd": 0,
-            "segmentations": [],
-            "keypoints": annot["keypoints"],
-            "num_keypoints": int(kp.shape[0]),
-        }
-        new_annot_file["annotations"].append(annot_result)
-    annot_id += 1
-
-with open("infant-dataset/annotations.json", "w", encoding="utf-8") as f:
-    json.dump(new_annot_file, f, indent="\t")
+    # with open("infant-dataset/annotations2.json", "w", encoding="utf-8") as f:
+    #     json.dump(new_annot_file, f, indent="\t")
 
 # paths = glob.glob("./original-data/*/*/*/*")
 # #
