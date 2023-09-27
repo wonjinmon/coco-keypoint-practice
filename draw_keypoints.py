5fb089a708decfd5f7ae29e6a81c23f8e2871b7d
wonjinmon <own1045@gmail.com>
2023-09-27 17:51:23 +0900

    add visualization

--- draw_keypoints.py
+++ draw_keypoints.py
@@ -9,7 +9,9 @@ import numpy as np
 with open("infant-dataset/annotations.json", "r") as f:
     data = json.load(f)
 
-sk_point = data["categories"][0]["skeleton"]
+# data["annotations"]["categories"]
+cats = data["categories"][0]["keypoints"]
+
 
 for idx, img_info in enumerate(data["images"]):
     img_path = "infant-dataset/" + img_info["file_name"]
@@ -20,14 +22,8 @@ for idx, img_info in enumerate(data["images"]):
     keypoints = np.array(keypoints).reshape(-1, 3)
 
     draw = ImageDraw.Draw(image)
-
-    for kp in keypoints:
+    for i, kp in tqdm(enumerate(keypoints)):
         x, y, _ = kp
         draw.ellipse((x - 5, y - 5, x + 5, y + 5), fill=(0, 0, 255), width=2)
-
-    for n1, n2 in tqdm(sk_point):
-        x1, y1, _ = keypoints[n1]
-        x2, y2, _ = keypoints[n2]
-        draw.line((x1, y1, x2, y2), fill=(0, 0, 255), width=2)
-
-    image.save(f"./skeleton/{idx}.jpg")
+        draw.text((x - 5, y - 5), str(i))
+    image.save(f'./keypoints/{img_info["id"]}_{str(idx+1)}.jpg')
